f9d6c2fb4e10fae470d973f8338f33dbf2dbfeec
robert-min <robertmin522@gmail.com>
2022-09-07 13:43:07 +0900

    activity 초기 환경 구축

---
+++ activity/views.py
@@ -0,0 +1,10 @@
+from .models import *
+
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+
+def show_activity(request):
+    return render(request, "activity/activity.html")
