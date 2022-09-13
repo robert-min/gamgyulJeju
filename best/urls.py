276c9aecd03f795ccf9de560f396379349125914
robert-min <robertmin522@gmail.com>
2022-09-13 10:14:59 +0900

    Best Page 프론트 작업 마무리

--- best/urls.py
+++ best/urls.py
@@ -7,4 +7,6 @@ app_name = "best"
 
 urlpatterns = [
     path('', show_best_pick, name='best_pick'),
+    path('keep/', show_best_keep, name='best_keep'),
+    path('food/', show_best_food, name='best_food')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- best/views.py
+++ best/views.py
@@ -9,3 +9,9 @@ from django.shortcuts import render, get_object_or_404
 
 def show_best_pick(request):
     return render(request, "best/pick.html")
+
+def show_best_keep(request):
+    return render(request, "best/keep.html")
+
+def show_best_food(request):
+    return render(request, "best/food.html")
